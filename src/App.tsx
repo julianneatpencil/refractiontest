513f7ce27ed9d7586ef991c360c5974c9b087a81
Julianne Lefelhocz <juliannelefelhocz@juliannes-air.myfiosgateway.com>
2025-03-06 10:11:58 -0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import { Canvas, useLoader } from '@react-three/fiber';
-import { MeshRefractionMaterial, Environment, OrbitControls } from '@react-three/drei';
+import { MeshRefractionMaterial, OrbitControls } from '@react-three/drei';
 import { RGBELoader } from 'three/examples/jsm/Addons.js';
 
-function Scene() {
-  const texture = useLoader(RGBELoader, `${import.meta.env.BASE_URL}/material/aerodynamics_workshop_1k.hdr`);
+function RefractionScene() {
+  const texture = useLoader(RGBELoader, `${import.meta.env.BASE_URL}material/aerodynamics_workshop_1k.hdr`);
+
   return (
     <Canvas shadows camera={{ position: [0, 0, 3], fov: 50 }}>
       <ambientLight intensity={0.5} />
       <directionalLight position={[2, 2, 2]} intensity={1} castShadow />
-
 
       <mesh castShadow receiveShadow>
         <icosahedronGeometry />
         <MeshRefractionMaterial envMap={texture} />
       </mesh>
 
+      <OrbitControls />
+    </Canvas>
+  );
+}
 
-      <Environment preset="studio" />
+function PlainScene() {
+  return (
+    <Canvas shadows camera={{ position: [0, 0, 3], fov: 50 }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 2, 2]} intensity={1} castShadow />
+
+      <mesh castShadow receiveShadow>
+        <icosahedronGeometry />
+        <meshStandardMaterial color="hotpink" />
+      </mesh>
+
       <OrbitControls />
     </Canvas>
   );
 }
 
 function App() {
-  return <Scene />;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around', width: '100vw', height: '100vh' }}>
+      <RefractionScene />
+      <PlainScene />
+    </div>
+  );
 }
 
 export default App;
